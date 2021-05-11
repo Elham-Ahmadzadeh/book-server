14baca96849d3f4501b8d7d6194af7457c497899
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-05-11 15:08:31 +0200

    fixed and cleaned code

--- controllers/books.js
+++ controllers/books.js
@@ -11,53 +11,68 @@ const connection = {
     trustServerCertificate: true,
   },
 }
-
-sql.connect(connection)
+sql.connect(connection);
  const getBookList = async (req, res) => {
   try {
-      const result = await sql.query(`
-                                          select ISBN13,
-                                                 title as Titel,
-                                                 Price_kr as Pris,
-                                                 FirstName  + ' ' + LastName as Författare
-                                                 from books left outer join Authors
-                                                 on Books.PublisherID = Authors.ID;`)
+    const query= `select
+    ISBN13,
+    title,
+    Price_kr,
+    ath.FirstName  + ' ' + ath.LastName as authors
+from books bk left outer join Authors ath
+    on bk.PublisherID = ath.ID`
+      const result = await sql.query(query)
       res.status(200).render('index.pug', {status: 'success', books: result.recordset })
     } catch (err) {
       console.log('ERROR!!!', err)
-      res.status(404).send(err.message)
+      res.status(500).send('Something went wrong!');
     }
 }
 const getOneBook = async (req, res) => {
   try {
-    const book = await sql.query(
-     `select ISBN13,
-     title as Titel, Language as språk,
-     Price_kr as Pris, FirstName + ' ' + LastName as Författare
-     from books left outer join Authors
-     on Books.PublisherID = Authors.ID
-     where ISBN13 =  ${req.params.isbn}`
-    )
-    const stockBalanceInfo = await sql.query(`
-    SELECT isnull(cast(Total as varchar),'ej i lager') as Total,
-    city, Name
-     FROM BookStores bk
-    OUTER APPLY (
-        SELECT *
-    FROM stockBalance st
-    WHERE bk.ID = st.StoreID
-        and st.ISBN = ${req.params.isbn}
-    ) AS T
-    `)
+   const queryBook = `select ISBN13,
+   title as Titel, Language as språk,
+   Price_kr as Pris, FirstName + ' ' + LastName as Författare
+   from books left outer join Authors
+   on Books.PublisherID = Authors.ID
+   where ISBN13 =  @ISBN`
+   const queryStockBalance = `select isnull(cast(Total as varchar),'ej i lager') as Total,
+   city, Name
+    from BookStores bk
+   outer apply (
+       select *
+   from stockBalance st
+   where bk.ID = st.StoreID
+       and st.ISBN = @ISBN
+   ) as T`
+
+   const configure =  await sql.connect(connection);
+    const book = await configure
+    .request()
+    .input('ISBN', sql.NVarChar, req.params.isbn)
+    .query(queryBook)
+    const stockBalanceInfo = await configure
+    .request()
+    .input('ISBN', sql.NVarChar, req.params.isbn)
+    .query(queryStockBalance)
+    if (book.recordset && book.recordset.length > 0 && stockBalanceInfo.recordset) {
     res.status(200).render('oneBook.pug', {status: 'success',
     books: book.recordset,
-    booksInfo: stockBalanceInfo.recordset})
+    stores: stockBalanceInfo.recordset})
+    } else {
+      res.status(404).send("The book is not found!");
+    }
   } catch (err) {
     console.log('ERROR!!!', err)
-    res.status(404).send(err.message)
+    res.status(500).send('Something went wrong!');
   }
 }
+const pageNotFound = (req, res) => {
+  res.status(404).send('<h1>Page not found</h1>')
+}
+
 module.exports = {
   getBookList,
-  getOneBook
+  getOneBook,
+  pageNotFound
 }
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const route = express.Router()
-
+//
 const {
   getBookList,
-  getOneBook
+  getOneBook,
+  pageNotFound
 } = require('../controllers/books')
 
 route.get('/',getBookList)
-
 route.get('/book/:isbn' , getOneBook)
-
+route.get('*', pageNotFound)
 module.exports = route
